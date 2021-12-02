7d55159d5f5811ac89ec2bd1cba884fa7836979e
gmrp22 <gmrp22@hotmail.com>
2021-12-01 22:06:39 -0600

    styles

--- index.js
+++ index.js
@@ -23,93 +23,94 @@ let currentRow;
 form.addEventListener("submit", onSubmit);
 
 /**
- * 
- * @param {Event} event 
+ *
+ * @param {Event} event
  */
-function onSubmit(event)
-{
-    event.preventDefault();
+function onSubmit(event) {
+  event.preventDefault();
 
-    const data = new FormData(form);
-    const values = Array.from(data.entries());
+  const data = new FormData(form);
+  const values = Array.from(data.entries());
 
-    const [frmCodigo, frmNombre, frmCantidad, frmPrecio, frmCategoria] = values;
+  const [frmCodigo, frmNombre, frmCantidad, frmPrecio, frmCategoria] = values;
 
-    let codigo = frmCodigo[1];
-    const nombre = frmNombre[1];
-    const cantidad = frmCantidad[1];
-    const precio = frmPrecio[1];
-    const categoria = frmCategoria[1];
-    const total = cantidad * precio;
+  let codigo = frmCodigo[1];
+  const nombre = frmNombre[1];
+  const cantidad = frmCantidad[1];
+  const precio = frmPrecio[1];
+  const categoria = frmCategoria[1];
+  const total = cantidad * precio;
 
-    cantidadTotal = parseFloat(cantidad) + cantidadTotal;
-    preciosTotales = parseFloat(precio) + preciosTotales;
-    granTotal = parseFloat(total) + granTotal;
+  cantidadTotal = parseFloat(cantidad) + cantidadTotal;
+  preciosTotales = parseFloat(precio) + preciosTotales;
+  granTotal = parseFloat(total) + granTotal;
 
-    let tr;
+  let tr;
 
-    if (!codigo)
-    {
-        indice++;
-        codigo = indice;
-        tr = document.createElement("tr");
-        tbody.appendChild(tr);
-    }
-    else
-    {
-        tr = currentRow;
-    }
-    
-    tr.dataset.categoria = categoria;
-    tr.innerHTML = `
+  if (!codigo) {
+    indice++;
+    codigo = indice;
+    tr = document.createElement("tr");
+    tbody.appendChild(tr);
+  } else {
+    tr = currentRow;
+  }
+
+  tr.dataset.categoria = categoria;
+  tr.innerHTML = `
         <td>${codigo}</td>
         <td>${nombre}</td>
         <td>${cantidad}</td>
         <td>${precio}</td>
         <td>${total}</td>
-        <td><a href="#" onclick="onEdit(event)">Editar</a> | <a href="#" onclick="onDelete(event)">Eliminar</a></td>
+        <td>
+            <div class="btn-group">
+                <a title="Editar" href="#" onclick="onEdit(event)" class="btn btn-sm btn-outline-secondary">
+                    <i class="bi bi-pencil-square"></i>
+                </a>
+                <a title="Eliminar" href="#" onclick="onDelete(event)" class="btn btn-sm btn-outline-danger">
+                    <i class="bi bi-trash"></i>
+                </a>
+            </div>
+        </td>
     `;
 
+  cantidadTotalElement.innerText = cantidadTotal;
+  precioTotalElement.innerText = preciosTotales;
+  granTotalElement.innerText = granTotal;
 
-    cantidadTotalElement.innerText = cantidadTotal;
-    precioTotalElement.innerText = preciosTotales;
-    granTotalElement.innerText = granTotal;
-
-    form.reset();
+  form.reset();
 }
 
 /**
- * 
- * @param {Event} event 
+ *
+ * @param {Event} event
  */
-function onEdit(event)
-{
-    event.preventDefault();
-    /** @type {HTMLElement} */
-    const anchor = event.target;
-    const tr = anchor.parentElement.parentElement;
-    const celdas = tr.getElementsByTagName("td");
-    const [tdCodigo, tdNombre, tdCantidad, tdPrecio] = celdas;
+function onEdit(event) {
+  event.preventDefault();
+  /** @type {HTMLElement} */
+  const anchor = event.currentTarget;
+  const tr = anchor.parentElement.parentElement.parentElement;
+  const celdas = tr.getElementsByTagName("td");
+  const [tdCodigo, tdNombre, tdCantidad, tdPrecio] = celdas;
 
-    inputCodigo.value = tdCodigo.innerText;
-    inputNombre.value = tdNombre.innerText;
-    inputCantidad.value = tdCantidad.innerText;
-    inputPrecio.value = tdPrecio.innerText;
-    selectCategoria.value = tr.dataset.categoria;
+  inputCodigo.value = tdCodigo.innerText;
+  inputNombre.value = tdNombre.innerText;
+  inputCantidad.value = tdCantidad.innerText;
+  inputPrecio.value = tdPrecio.innerText;
+  selectCategoria.value = tr.dataset.categoria;
 
-    currentRow = tr;
+  currentRow = tr;
 }
 
 /**
- * 
- * @param {Event} event 
+ *
+ * @param {Event} event
  */
-function onDelete(event)
-{
-    event.preventDefault();
-
-    /** @type {HTMLElement} */
-    const anchor = event.target;
-    const tr = anchor.parentElement.parentElement;
-    tbody.removeChild(tr);
+function onDelete(event) {
+  event.preventDefault();
+  /** @type {HTMLElement} */
+  const anchor = event.currentTarget;
+  const tr = anchor.parentElement.parentElement.parentElement;
+  tbody.removeChild(tr);
 }
